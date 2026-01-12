68d33928cc3723d2e566ef1863d8546f565deb71
Radeshgokul potti <radeshgokul17@gmail.com>
2026-01-13 01:44:22 +0530

    Trigger Deployment: Force fresh build for Vercel/Render

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+// Deployment Pulse: Hardened Initialization + Manual Recovery V1.2
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
